ddf6c0213ad0c5bd87b6719f748f908818081e10
WC Lovers <45789664+wclovers@users.noreply.github.com>
2023-12-20 23:53:11 +0530

    Show loading skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,22 +24,28 @@ interface GamesResponse {
 const useGames = () => {
 	const [games, setGames] = useState<Game[]>([]);
 	const [error, setError] = useState("");
+	const [isLoading, setLoading] = useState(false);
 
 	const controller = new AbortController();
 
 	useEffect(() => {
+		setLoading(true);
 		apiClient
 			.get<GamesResponse>("/games", { signal: controller.signal })
-			.then((response) => setGames(response.data.results))
+			.then((response) => {
+				setGames(response.data.results);
+				setLoading(false);
+			})
 			.catch((error) => {
 				if (error instanceof CanceledError) return;
 				setError(error.message);
+				setLoading(false);
 			});
 
 		// return () => controller.abort();
 	}, []);
 
-	return { games, error };
+	return { games, error, isLoading };
 };
 
 export default useGames;
